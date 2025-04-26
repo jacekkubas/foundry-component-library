afd590e33d17f0a8dc9c6f5313aa3ff71868a381
Jacek Kubas <jacenty.kubas@gmail.com>
2025-04-26 12:50:34 +0200

    add CaseStudyTeaser

---
+++ lib/components/CaseStudyTeaser/index.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { translate } from "../../utils";
+import styles from "./styles.module.scss";
+import Container from "../Container";
+import { Case, NextImage } from "../../types";
+import useDrag from "../../hooks/useDrag";
+
+const CaseStudyTeaser = ({
+  heading,
+  cases,
+  Image,
+}: {
+  heading: string;
+  cases: Case[];
+  Image: NextImage;
+}) => {
+  const headingRef = useRef<HTMLDivElement>(null);
+  const casesRef = useRef<HTMLDivElement>(null);
+  const [padding, setPadding] = useState(0);
+  const { handleMouseDown, handleMouseMove, handleMouseUp, dragStyle } =
+    useDrag(casesRef);
+
+  useEffect(() => {
+    if (headingRef.current) {
+      setPadding(headingRef.current.offsetLeft);
+    }
+  }, [headingRef]);
+
+  if (!cases) return;
+
+  return (
+    <div className={styles.caseStudyTeaser}>
+      <Container>
+        <div ref={headingRef} className={styles.heading}>
+          {heading}
+        </div>
+      </Container>
+
+      <div
+        className={styles.cases}
+        ref={casesRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseUp}
+        style={{
+          ...(dragStyle as React.CSSProperties),
+          paddingLeft: padding,
+          paddingRight: padding,
+        }}
+      >
+        {cases.map((el) => {
+          return (
+            <div key={el.id} className={styles.case}>
+              <Image
+                className={styles.image}
+                src={el.case?.mainImage?.sourceUrl || ""}
+                alt={el.title}
+                fill
+              />
+              <div className={styles.texts}>
+                <div className={styles.caption}>{el.case?.caption}</div>
+                <div className={styles.title}>{el.title}</div>
+                <a href={el.uri} className={styles.button}>
+                  {translate("See More")}
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CaseStudyTeaser;
