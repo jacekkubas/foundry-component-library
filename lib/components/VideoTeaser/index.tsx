a07e8cfaa53eba490ea79eb97cd7fa96f5886667
Jacek Kubas <jacenty.kubas@gmail.com>
2025-05-06 12:35:56 +0200

    add Mask to VideoTeaser

--- lib/components/VideoTeaser/index.tsx
+++ lib/components/VideoTeaser/index.tsx
@@ -5,11 +5,13 @@ import styles from "./styles.module.scss";
 import Mute from "../../assets/svg/mute.svg";
 import Muted from "../../assets/svg/muted.svg";
 import PlayButton from "../../assets/svg/play-button.svg";
+import Mask from "./Mask";
 
 function VideoTeaser({ url }: { url: string }) {
   const [playing, setPlaying] = useState(false);
   const [hasWindow, setHasWindow] = useState(false);
   const [isMuted, setIsMuted] = useState(true);
+  const [isAnimated, setIsAnimated] = useState(false);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -30,12 +32,15 @@ function VideoTeaser({ url }: { url: string }) {
         playing ? styles.playing : ""
       }`}
     >
+      <Mask isAnimated={isAnimated} setIsAnimated={setIsAnimated} />
       <button className={styles.btnMute} onClick={() => setIsMuted(!isMuted)}>
         {isMuted ? <Muted /> : <Mute />}
       </button>
       <div className={styles.video} onClick={handleClick}>
         <div
-          className={`${styles.playButton} ${playing ? styles.playing : ""}`}
+          className={`${styles.playButton} ${playing ? styles.playing : ""} ${
+            isAnimated ? styles.isAnimated : ""
+          }`}
         >
           <PlayButton />
         </div>
