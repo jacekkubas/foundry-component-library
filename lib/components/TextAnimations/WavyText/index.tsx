5d7d1e675bc952a8c710fbb77720595200a653f5
Jacek Kubas <jacenty.kubas@gmail.com>
2025-05-08 10:47:37 +0200

    add TextAnimations

---
+++ lib/components/TextAnimations/WavyText/index.tsx
@@ -0,0 +1,67 @@
+import { FC } from "react";
+import { motion, Variants, HTMLMotionProps } from "framer-motion";
+
+interface Props extends HTMLMotionProps<"div"> {
+  text: string;
+  delay?: number;
+  duration?: number;
+}
+
+const WavyText: FC<Props> = ({
+  text,
+  delay = 0,
+  duration = 0.05,
+  ...props
+}: Props) => {
+  const letters = text.split("");
+
+  const container: Variants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: (i: number = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: duration, delayChildren: i * delay },
+    }),
+  };
+
+  const child: Variants = {
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 200,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      y: 20,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 200,
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      style={{ display: "flex", overflow: "hidden" }}
+      variants={container}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, margin: "0px 0px -10% 0px" }}
+      {...props}
+    >
+      {letters.map((letter, index) => (
+        <motion.span key={index} variants={child}>
+          {letter === " " ? "\u00A0" : letter}
+        </motion.span>
+      ))}
+    </motion.div>
+  );
+};
+
+export default WavyText;
